4c4e5ff0d2bfc6e056e8d77ec227fccd2eb2b14d
Jake Barnes <jakerodneybarnes@gmail.com>
2016-08-18 05:35:22 +1000

    Update test code to fetch subscriptions, batch fetching uploads, and handle durations with no seconds

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,62 +1,85 @@
 require 'google/apis/youtube_v3'
 
 class WelcomeController < ApplicationController
-  def index
-    youtube = Google::Apis::YoutubeV3::YouTubeService.new
-    youtube.authorization = nil
-    youtube.key = ENV["GOOGLE_API_KEY"]
+  acts_as_token_authentication_handler_for User
 
-    @channels = [{ id: 'UCAWQEAjn8udSFKN6D4NlqWQ' }, { id: 'UCFKDEp9si4RmHFWJW1vYsMA' }, { id: 'UC3tNpTOHsTnkmbwztCs30sA' }]
+  def index
+    authorization = Signet::OAuth2::Client.new(
+      authorization_uri: 'https://accounts.google.com/o/oauth2/auth',
+      token_credential_uri: 'https://www.googleapis.com/oauth2/v3/token',
+      client_id: ENV['GOOGLE_CLIENT_ID'],
+      client_secret: ENV['GOOGLE_CLIENT_SECRET'],
+      access_token: current_user.access_token,
+      refresh_token: current_user.refresh_token,
+      expires_at: current_user.expires_at
+    )
+
+    # call this and save changes back to DB
+    #authorization.refresh!
+
+    youtube = Google::Apis::YoutubeV3::YouTubeService.new
+    youtube.authorization = authorization
+
+    @channels = []
     @videos = []
+
+    # get subscriptions
+    youtube.fetch_all do |token|
+      youtube.list_subscriptions('snippet', mine: true, max_results: 50, page_token: token)
+    end.each do |item|
+      @channels << {
+        id: item.snippet.resource_id.channel_id,
+        title: item.snippet.title,
+        thumbnail: item.snippet.thumbnails.default.url,
+        checked: DateTime.now - 1.day
+      }
+    end
 
     # get channel data, updated infrequently
     youtube.batch do |youtube|
       @channels.each do |channel|
-        youtube.list_channels('snippet,contentDetails', id: channel[:id]) do |result|
+        youtube.list_channels('snippet,contentDetails', id: channel[:id]) do |result, err|
           item = result.items.first
           channel[:title] = item.snippet.title
           channel[:thumbnail] = item.snippet.thumbnails.default.url
           channel[:uploads] = item.content_details.related_playlists.uploads
-          channel[:checked] = DateTime.now - 4.days # XXX arbitrary initial limit on videos to fetch
         end
       end
     end
 
     # get new videos
-    @channels.each do |channel|
-      result = youtube.list_playlist_items('snippet', playlist_id: channel[:uploads], max_results: 2)
-      while true
-        added = 0
-        result.items.each do |item|
-          break if item.snippet.published_at.to_datetime < channel[:checked]
-          @videos << {
-            id: item.snippet.resource_id.video_id,
-            channel: channel[:id],
-            published_at: item.snippet.published_at.to_datetime,
-            title: item.snippet.title,
-            thumbnail: item.snippet.thumbnails.default.url
-          }
-          added += 1
+    @to_check = @channels.clone
+    @to_check_token = {}
+    while @to_check.count > 0 do
+      youtube.batch do |youtube|
+        @to_check.each do |channel|
+          youtube.list_playlist_items('snippet', playlist_id: channel[:uploads], max_results: 2, page_token: @to_check_token[channel[:id]]) do |result, err|
+            @to_check_token[channel[:id]] = result.next_page_token
+            added = false
+            result.items.each do |item|
+              break if item.snippet.published_at.to_datetime < channel[:checked]
+              added = true
+              @videos << {
+                id: item.snippet.resource_id.video_id,
+                channel: channel[:id],
+                published_at: item.snippet.published_at.to_datetime,
+                title: item.snippet.title,
+                thumbnail: item.snippet.thumbnails.default.url
+              }
+            end
+            @to_check.delete channel unless added
+          end
         end
-
-        # hit the end
-        break if added < 2 || result.next_page_token.nil?
-
-        # get next page
-        result = youtube.list_playlist_items 'snippet',
-          playlist_id: channel[:uploads],
-          max_results: 2,
-          page_token: result.next_page_token
       end
     end
 
     # get duration for each video
     youtube.batch do |youtube|
       @videos.each do |video|
-        youtube.list_videos('snippet,contentDetails', id: video[:id]) do |result|
+        youtube.list_videos('contentDetails', id: video[:id]) do |result, err|
           item = result.items.first
-          captures = item.content_details.duration.match(/PT((\d+)H)?((\d+)M)?(\d+)S/).captures
-          video[:duration] = (captures[1].nil? ? 0 : captures[1].to_i.hours) + (captures[3].nil? ? 0 : captures[3].to_i.minutes) + captures[4].to_i.seconds
+          captures = item.content_details.duration.match(/PT((\d+)H)?((\d+)M)?((\d+)S)?/).captures
+          video[:duration] = (captures[1].nil? ? 0 : captures[2].to_i.hours) + (captures[3].nil? ? 0 : captures[4].to_i.minutes) + (captures[5].nil? ? 0 : captures[6].to_i.seconds)
         end
       end
     end
