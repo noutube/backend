04342c4df8554013a5b7b2cf65a0341e9bf2fc36
Jake Barnes <jakerodneybarnes@gmail.com>
2017-03-29 19:44:29 +1100

    Move filtering logic out off display-subscription

--- frontend/app/components/display-subscription/component.js
+++ frontend/app/components/display-subscription/component.js
@@ -2,12 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   subscription: null,
-  state: '',
-
-  items: Ember.computed('subscription.items.@each.state', 'state', function() {
-    let items = this.get('state') === 'new'   ? this.get('subscription.newItems')   :
-                this.get('state') === 'later' ? this.get('subscription.laterItems') :
-                                                this.get('subscription.items');
-    return items;
-  })
+  items: null
 });
--- frontend/app/components/route-index/component.js
+++ frontend/app/components/route-index/component.js
@@ -1,6 +1,18 @@
 import Ember from 'ember';
+import DS from 'ember-data';
 
 export default Ember.Component.extend({
   items: null,
-  subscriptions: null
+  subscriptions: null,
+
+  newSubscriptions: Ember.computed('subscriptions.@each.newItems', function() {
+    return DS.PromiseArray.create({
+      promise: Ember.RSVP.filter(this.get('subscriptions').toArray(), subscription => subscription.get('newItems').then(items => items.length > 0))
+    });
+  }),
+  laterSubscriptions: Ember.computed('subscriptions.@each.laterItems', function() {
+    return DS.PromiseArray.create({
+      promise: Ember.RSVP.filter(this.get('subscriptions').toArray(), subscription => subscription.get('laterItems').then(items => items.length > 0))
+    });
+  })
 });
