5450d5e5312e03d0b0e36542d1786c526227c593
Jake Barnes <jakerodneybarnes@gmail.com>
2017-04-04 12:07:34 +1000

    Display total duration of items for a subscription

--- frontend/app/components/display-subscription/component.js
+++ frontend/app/components/display-subscription/component.js
@@ -2,5 +2,9 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   subscription: null,
-  items: null
+  items: null,
+
+  totalDuration: Ember.computed('items.[]', function() {
+    return this.get('items').map(item => item.get('video.duration')).reduce((acc, n) => acc + n, 0);
+  })
 });
