5c4d9598066f16544df74c0a2c92623458a06b38
Jake Barnes <jakerodneybarnes@gmail.com>
2017-05-27 15:04:47 +1000

    Add bulk operations

--- frontend/app/components/display-subscription/component.js
+++ frontend/app/components/display-subscription/component.js
@@ -6,5 +6,23 @@ export default Ember.Component.extend({
 
   totalDuration: Ember.computed('items.[]', function() {
     return this.get('items').map((item) => item.get('video.duration')).reduce((acc, n) => acc + n, 0);
-  })
+  }),
+
+  isNew: Ember.computed('items.[]', function() {
+    return this.get('items').any((item) => item.get('isNew'));
+  }),
+
+  actions: {
+    markAllLater() {
+      this.get('items').forEach((item) => {
+        item.set('state', 'state_later');
+        item.save().catch(() => {
+          item.rollbackAttributes();
+        });
+      });
+    },
+    destroyAll() {
+      this.get('items').invoke('destroyRecord');
+    }
+  }
 });
