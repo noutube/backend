6dedfda4a76ae5f7b0a2395998968e32b914221c
Jake Barnes <jakerodneybarnes@gmail.com>
2017-06-11 03:34:02 +1000

    Remove display-section, add no videos section

--- frontend/app/components/route-index/component.js
+++ frontend/app/components/route-index/component.js
@@ -4,6 +4,28 @@ export default Ember.Component.extend({
   items: null,
   subscriptions: null,
 
+  processSubscriptions(state) {
+    return this.get('subscriptions')
+      .filter((subscription) => subscription.get('items').filter((item) => item.get(state)).length > 0)
+      .sort((subscriptionA, subscriptionB) => {
+        let titleA = subscriptionA.get('channel.title').toLowerCase();
+        let titleB = subscriptionB.get('channel.title').toLowerCase();
+        return titleA.localeCompare(titleB);
+      });
+  },
+
+  newSubscriptions: Ember.computed('subscriptions.[]', 'items.@each.new', function() {
+    return this.processSubscriptions('new');
+  }),
+  laterSubscriptions: Ember.computed('subscriptions.[]', 'items.@each.later', function() {
+    return this.processSubscriptions('later');
+  }),
+  noItems: Ember.computed('items.@each.{new,later}', function() {
+    return this.get('items')
+      .filter((item) => item.get('new') || item.get('later'))
+      .length == 0;
+  }),
+
   didInsertElement() {
     Ember.$(document).on('keyup', this.onKeyUp.bind(this));
   },
