1ba72c4a6fff132e4746a507a1f55c5849a0146a
Jake Barnes <me@jakebarn.es>
2019-06-01 01:51:57 +1000

    Allow deleting user from privacy page

---
+++ frontend/app/components/route-privacy/component.js
@@ -0,0 +1,21 @@
+import Component from '@ember/component';
+import { action, set } from '@ember/object';
+import { inject as service } from '@ember/service';
+
+export default class RoutePrivacyComponent extends Component {
+  @service session;
+  @service router;
+
+  @action
+  async destroyMe() {
+    let { me } = this.session;
+    try {
+      me.deleteRecord();
+      await me.save();
+      set(this.session, 'me', null);
+      this.router.transitionTo('landing');
+    } catch (e) {
+      me.rollbackAttributes();
+    }
+  }
+}
