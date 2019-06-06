95561e57bbc33f1c84c38030f77714500241aeef
Jake Barnes <me@jakebarn.es>
2019-06-06 21:12:04 +1000

    Move frontend to separate repo

--- frontend/app/components/app-footer/component.js
+++
@@ -1,8 +0,0 @@
-import Component from '@ember/component';
-
-import { classNames } from '@ember-decorators/component';
-
-export default
-@classNames('footer')
-class AppFooterComponent extends Component {
-}
--- frontend/app/components/section-header/component.js
+++
@@ -1,9 +0,0 @@
-import Component from '@ember/component';
-
-import { classNames, tagName } from '@ember-decorators/component';
-
-export default
-@classNames('section')
-@tagName('p')
-class SectionHeaderComponent extends Component {
-}
