650e9602e71e1366baaa121c488021dab8d9fab4
Jake Barnes <me@jakebarn.es>
2019-12-28 15:01:46 +1100

    Update to Rails 6

--- Gemfile
+++ Gemfile
@@ -3,14 +3,14 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '~> 2.6.0'
 
-gem 'rails', '~> 5.2.4', '>= 5.2.4.1'
+gem 'rails', '~> 6.0.2', '>= 6.0.2.1'
 
 gem 'active_model_serializers', '~> 0.10.0'
 gem 'cancancan'
 gem 'google-api-client'
 gem 'pg'
 gem 'rack-cors'
-gem 'redis'
+gem 'redis', '~> 4.0'
 gem 'signet'
 
 # development
@@ -27,6 +27,6 @@ group :development, :test do
 end
 
 # deployment
-gem 'puma'
+gem 'puma', '~> 4.1'
 gem 'dotenv-rails'
-gem 'bootsnap', '>= 1.1.0', require: false
+gem 'bootsnap', '>= 1.4.2', require: false
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,6 +18,7 @@ Rails.application.configure do
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp', 'caching-dev.txt').exist?
     config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
@@ -43,16 +44,17 @@ Rails.application.configure do
   # Highlight code that triggered database queries in logs.
   config.active_record.verbose_query_logs = true
 
-  # Raises error for missing translations
+  # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  config.action_cable.allowed_request_origins = ['http://localhost:4200', 'http://192.168.1.100:4200']
   config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:4200'
+      origins 'http://localhost:4200', 'http://192.168.1.100:4200'
       resource '*', headers: :any, methods: :any
     end
   end
