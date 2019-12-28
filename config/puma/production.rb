0d2861a7438af246832dcf49189d789964953c8f
Jake Barnes <me@jakebarn.es>
2019-12-28 15:53:22 +1100

    Drop puma production config

--- config/puma/production.rb
+++
@@ -1,7 +0,0 @@
-directory '/root/nou2ube'
-environment 'production'
-daemonize true
-pidfile 'tmp/pids/puma.pid'
-state_path 'tmp/pids/puma.state'
-stdout_redirect 'log/puma.stdout.log', 'log/puma.stderr.log', true
-bind 'unix:///root/nou2ube/tmp/sockets/puma.sock'
