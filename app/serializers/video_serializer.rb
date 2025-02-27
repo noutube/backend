6855c0b2fe2e44946a251311d50207a6032ecdea
Jake Barnes <me@jakebarn.es>
2025-02-27 13:40:56 +1100

    Expose visibility for videos

--- app/serializers/video_serializer.rb
+++ app/serializers/video_serializer.rb
@@ -1,5 +1,5 @@
 class VideoSerializer < ActiveModel::Serializer
-  attributes :id, :api_id, :title, :thumbnail, :duration, :published_at, :is_live, :is_live_content, :is_upcoming, :scheduled_at
+  attributes :id, :api_id, :title, :thumbnail, :duration, :published_at, :is_live, :is_live_content, :is_upcoming, :scheduled_at, :visibility
 
   attribute :state do
     Item.find_by(video: object, user: scope).state
